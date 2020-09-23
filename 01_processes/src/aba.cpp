88805b5a4c8f3a0ea5de8b02aac25c46fbf4c49f
David Fischer <david@konst.fish>
2020-09-23 16:05:50 +0200

    Add 2. to 7. subexercises

--- 01_processes/src/aba.cpp
+++ 01_processes/src/aba.cpp
@@ -1,34 +1,66 @@
 /*
     FISCHER David
 
+Notes:
+ps -alx | grep aba | grep -v Chrome | grep -v grep
+
 Antworten:
 
 1.) Die ausgabe ist fast unvorhersehbar da wir keinen wirklichen
     Rechenaufwand haben.
+
+6.) Ich bekomme es beim besten willen nicht zusammen einen Zombie
+    zu erzeugen. Könnte an macOS liegen, bin mir aber nicht sicher.
+    Jedenfalls sollte ein Zombie entstehen da nie waitpid() aufgerufen
+    wird.
+
+    Um zu überprüfen, ob ein Prozess zum Zombie geworden ist, reicht der 
+    command 'ps <PID des Prozesses>' also zum Beispiel `ps 123`
+
+7.) Output:
+    Child: 17048
+    BAABABABABABA
+    Status: 9 <------ Status ist 9, da das SIGKILL Signal gesendet wird.
+
 */
 
 #include <iostream>
 #include <unistd.h>
 #include <cstdlib>
+#include <thread>
+#include <chrono>
+#include <signal.h>
 
 using namespace std;
 
 int main() {
     auto pid{fork()};
+    std::chrono::milliseconds sleeptime(500);
 
     if (pid == 0) {
-        // parent
+        // child
         while (true) {
             cout << "A";
             cout << flush;
-            sleep(1);
+            this_thread::sleep_for(sleeptime);
         }
     } else {
-        // child
+        // parent
+        int iterations = 0;
+        cout << "Child: " << pid << endl;
+
         while (true) {
+            iterations += 1;
             cout << "B";
             cout << flush;
-            sleep(1);
+            this_thread::sleep_for(sleeptime);
+            if(iterations == 6){
+                kill(pid, SIGKILL);
+                int status;
+                waitpid(pid, &status, 0);
+                cout << endl << "Status: " << status;
+                exit(0);
+            }
         }
     }
 
