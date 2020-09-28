e6de5627cea34c336a1b307a88c0c0364bed17f2
David Fischer <david@konst.fish>
2020-09-28 19:14:44 +0200

    Add Subexercise 2. to 4. (Clean up Output, Add operator to class)

--- 02_threads/src/main.cpp
+++ 02_threads/src/main.cpp
@@ -1,14 +1,42 @@
 #include <iostream>
 #include <thread>
+#include <random>
 
 using namespace std;
 
 void mini_jcw();
 
+class Car{
+    public:
+    string name;
+
+    Car(string n){
+        name = n;
+    }
+
+    void operator()(){
+        int round = 0;
+        string tmp;
+        while (true) {
+            //dis(gen)
+            this_thread::sleep_for(1s);
+            round += 1;
+            tmp = to_string(round) + " " + name + "\n";
+            cout << tmp;     
+        }
+    }
+};
+
 int main() {
-    thread t{mini_jcw};
-    t.join();
-    cout << t.joinable() << endl;
+    Car c1("MINI Cooper S");
+    Car c2("MINI JCW");
+
+    thread t1{c1};
+    thread t2{c2};
+    
+    t1.join();
+    t2.join();
+    //thread t2{c2};
 }
 
 void mini_jcw(){
