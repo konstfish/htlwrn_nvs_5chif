bb87e6489f0330eaa5d19212e2e945e51ffdf82a
David Fischer <david@konst.fish>
2020-12-06 14:46:32 +0100

    Subexercise 6. - Add TimeMaster class & Implement instance in main()

--- 07_berkeley/src/main.cpp
+++ 07_berkeley/src/main.cpp
@@ -24,12 +24,35 @@ class TimeSlave{
     };
 };
 
+class TimeMaster{
+    private:
+
+    std::string name;
+    Clock c;
+
+    public:
+
+    TimeMaster(std::string rn, int hours, int minutes, int seconds):name( rn ){
+        c = Clock(name, hours, minutes, seconds);
+    };
+
+    void operator()(){
+        thread clock{c};
+
+        clock.join();
+    };
+};
+
 int main() {
     thread s1{TimeSlave("slave1", 1, 1, 1)};
     thread s2{TimeSlave("slave2", 1, 1, 1)};
 
+    thread m1{TimeMaster("master1", 1, 1, 1)};
+
     s1.join();
     s2.join();
+
+    m1.join();
 
     return 0;
 }
