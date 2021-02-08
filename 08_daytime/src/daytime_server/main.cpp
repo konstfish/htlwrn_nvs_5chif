ba54fbd75bde8da2997a46dc355f443922b72f23
David Fischer <david@konst.fish>
2021-02-08 16:37:36 +0100

    Implement Subexercise 9 - Add Simple Daytime server

--- 08_daytime/src/daytime_server/main.cpp
+++ 08_daytime/src/daytime_server/main.cpp
@@ -3,12 +3,52 @@
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 
+#include <asio.hpp>
+
+#include "CLI11.hpp"
+
+#include <iomanip>
+#include <ctime>
+
+using namespace asio::ip;
 using namespace std;
 
-int main() {
+int main(int argc, char** argv) {
     auto console = spdlog::stdout_color_mt("console");
-    
-    spdlog::get("console")->info("test!");
+
+    spdlog::get("console")->info("Starting Daytime Server!");
+
+    CLI::App app("Daytime Server");
+
+    int port{1113};
+
+    app.add_option("port", port, "server port");
+
+    try {
+        app.parse(argc, argv);
+    } catch (const CLI::ParseError &e) { 
+        spdlog::get("console")->error("Error while Parsing CLI Arguments");
+
+        return app.exit(e);
+    }
+
+    spdlog::get("console")->info("CLI Arguments sucessfully parsed");
+
+    spdlog::get("console")->info("Starting server on port {}.", port);
+
+    asio::io_context ctx;
+    tcp::endpoint ep{tcp::v4(), 1113}; 
+    tcp::acceptor acceptor{ctx, ep}; // IO object acceptor.listen();
+    tcp::socket sock{ctx};
+    acceptor.accept(sock);
+    tcp::iostream strm{std::move(sock)};
+    //shorter: tcp::iostream strm{acceptor.accept()};
+
+    auto t = std::time(nullptr);
+    auto tm = *std::localtime(&t);
+
+    strm << std::put_time(&tm, "%d-%m-%Y %H-%M-%S");
+    strm.close();
 
     return 0;
 }
