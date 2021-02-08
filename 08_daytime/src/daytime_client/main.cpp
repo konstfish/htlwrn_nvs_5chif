e230ddd11ba072e19bd80b7515feb8fa6bbf23f8
David Fischer <david@konst.fish>
2021-02-08 16:04:07 +0100

    Add Subexercise 3 & 4 (Include asio)

--- 08_daytime/src/daytime_client/main.cpp
+++ 08_daytime/src/daytime_client/main.cpp
@@ -3,11 +3,18 @@
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 
+#include <asio.hpp>
+
 using namespace std;
 
 int main() {
     auto console = spdlog::stdout_color_mt("console");
     
+    asio::io_context io;
+    asio::steady_timer t(io, asio::chrono::seconds(5));
+
+    t.wait();
+
     spdlog::get("console")->info("test!");
 
     return 0;
