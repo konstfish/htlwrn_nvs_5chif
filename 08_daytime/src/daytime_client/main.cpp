2244f6287e9e53f9cee1bc99065d6ef321ea14e8
David Fischer <david@konst.fish>
2021-02-08 16:15:10 +0100

    Add Subexercise 5 - (Implement stream based client to retrieve date from daytime server)

--- 08_daytime/src/daytime_client/main.cpp
+++ 08_daytime/src/daytime_client/main.cpp
@@ -5,17 +5,33 @@
 
 #include <asio.hpp>
 
+using namespace asio::ip;
 using namespace std;
 
 int main() {
     auto console = spdlog::stdout_color_mt("console");
     
+    /*
     asio::io_context io;
     asio::steady_timer t(io, asio::chrono::seconds(5));
 
     t.wait();
 
     spdlog::get("console")->info("test!");
+    */
+
+    tcp::iostream strm{"localhost", "1113"};
+
+
+
+    if(strm) {
+        string data;
+        getline(strm, data);
+        cout << data << endl;
+        strm.close();
+    } else { 
+        cout << "no connection" << endl; 
+    }
 
     return 0;
 }
